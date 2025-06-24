2f54e4c6d93dc0357e43a8dea363c2eecd388942
anderson <andersondiasmd25@gmail.com>
2025-06-24 17:35:42 -0300

    add dados dinamicos na dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,14 @@
-export default function DashboardPage() {
-  return <div>Ola cheguei</div>;
+import { auth } from "@/lib/auth";
+
+export default async function DashboardPage() {
+  const session = await auth();
+
+  if (!session?.user) return null;
+
+  return (
+    <div className="">
+      <h1>Olá {session.user.name}</h1>
+      <p>{session.user.email}</p>
+    </div>
+  );
 }

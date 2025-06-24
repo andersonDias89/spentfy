70320e5f2c5a18cc8350f0ce6573db473b3bec67
anderson <andersondiasmd25@gmail.com>
2025-06-24 17:22:24 -0300

    logando com github concluido

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/common/ui/Button";
+import { signIn } from "@/lib/auth";
 import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
+  async function handleRegister() {
+    "use server";
+    await signIn("github", { redirectTo: "/dashboard" });
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button icon={<FaGithub size={20} />}>Entrar com GitHub</Button>
+      <Button onClick={handleRegister} icon={<FaGithub size={20} />}>
+        Entrar com GitHub
+      </Button>
     </div>
   );
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,8 @@ import { prisma } from "@/lib/prisma";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  secret: process.env.AUTH_SECRET!,
+  trustHost: true,
   adapter: PrismaAdapter(prisma),
   providers: [GitHub],
 });
